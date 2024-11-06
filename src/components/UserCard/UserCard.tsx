368d1c41bfedfaead19da40963454d4fa24c4978
Pavel <pavlo.varshavskyi@materials.zone>
2024-11-06 23:28:30 +0200

    Code styling

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -39,90 +39,91 @@ const UserCard: React.FC<UserCardProps> = memo(({ user }) => {
 
     return (
         <LazyRenderer>
-        <Card
-            variant="outlined"
-            sx={{
-                boxShadow: '0 4px 15px rgba(0, 0, 0, 0.1)',
-                transition: 'box-shadow 0.3s ease',
-                '&:hover': {
-                    boxShadow: (theme) => `0 4px 10px ${theme.palette.primary.main}50`,
-                },
-            }}
-        >
-            <CardContent
+            <Card
+                variant="outlined"
                 sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'flex-end',
-                    gap: '10px',
+                    contentVisibility: 'auto',
+                    boxShadow: '0 4px 15px rgba(0, 0, 0, 0.1)',
+                    transition: 'box-shadow 0.3s ease',
+                    '&:hover': {
+                        boxShadow: (theme) => `0 4px 10px ${theme.palette.primary.main}50`,
+                    },
                 }}
             >
-                <Avatar
-                    alt={user.name}
-                    src={user.image}
-                    sx={{ width: 140, height: 140 }}
-                />
+                <CardContent
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'flex-end',
+                        gap: '10px',
+                    }}
+                >
+                    <Avatar
+                        alt={user.name}
+                        src={user.image}
+                        sx={{ width: 140, height: 140 }}
+                    />
 
-                <Box>
-                    <Typography variant="h6">{user.name}</Typography>
-                    <Select
-                        value={user.status}
-                        onChange={handleSelectChange}
-                        variant="standard"
-                        sx={{
-                            width: 150,
-                            '& .MuiSelect-select': {
-                                display: 'flex',
-                                flexDirection: 'row',
-                            },
-                            '& .MuiListItemIcon-root': {
-                                minWidth: 28,
-                                alignItems: 'center',
-                            }
-                        }}
-                    >
-                        <MenuItem
-                            value="Working"
+                    <Box>
+                        <Typography variant="h6">{user.name}</Typography>
+                        <Select
+                            value={user.status}
+                            onChange={handleSelectChange}
+                            variant="standard"
+                            sx={{
+                                width: 150,
+                                '& .MuiSelect-select': {
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                },
+                                '& .MuiListItemIcon-root': {
+                                    minWidth: 28,
+                                    alignItems: 'center',
+                                }
+                            }}
                         >
-                            <ListItemIcon>
-                                <TripOriginIcon sx={{ height:12, color: red[500]}} />
-                            </ListItemIcon>
+                            <MenuItem
+                                value="Working"
+                            >
+                                <ListItemIcon>
+                                    <TripOriginIcon sx={{ height:12, color: red[500]}} />
+                                </ListItemIcon>
 
-                            <Typography>
-                                Working
-                            </Typography>
-                        </MenuItem>
-                        <MenuItem value="On Vacation">
-                            <ListItemIcon>
-                                <TripOriginIcon sx={{ height:12, color: blue[500]}} />
-                            </ListItemIcon>
+                                <Typography>
+                                    Working
+                                </Typography>
+                            </MenuItem>
+                            <MenuItem value="On Vacation">
+                                <ListItemIcon>
+                                    <TripOriginIcon sx={{ height:12, color: blue[500]}} />
+                                </ListItemIcon>
 
-                            <Typography>
-                                On Vacation
-                            </Typography>
-                        </MenuItem>
-                        <MenuItem value="Lunch Time">
-                            <ListItemIcon>
-                                <TripOriginIcon sx={{ height:12, color: green[500]}} />
-                            </ListItemIcon>
+                                <Typography>
+                                    On Vacation
+                                </Typography>
+                            </MenuItem>
+                            <MenuItem value="Lunch Time">
+                                <ListItemIcon>
+                                    <TripOriginIcon sx={{ height:12, color: green[500]}} />
+                                </ListItemIcon>
 
-                            <Typography>
-                                Lunch Time
-                            </Typography>
-                        </MenuItem>
-                        <MenuItem value="Business Trip">
-                            <ListItemIcon>
-                                <TripOriginIcon sx={{ height:12, color: yellow[500]}} />
-                            </ListItemIcon>
+                                <Typography>
+                                    Lunch Time
+                                </Typography>
+                            </MenuItem>
+                            <MenuItem value="Business Trip">
+                                <ListItemIcon>
+                                    <TripOriginIcon sx={{ height:12, color: yellow[500]}} />
+                                </ListItemIcon>
 
-                            <Typography>
-                                Business Trip
-                            </Typography>
-                        </MenuItem>
-                    </Select>
-                </Box>
-            </CardContent>
-        </Card>
+                                <Typography>
+                                    Business Trip
+                                </Typography>
+                            </MenuItem>
+                        </Select>
+                    </Box>
+                </CardContent>
+            </Card>
         </LazyRenderer>
     );
 });
